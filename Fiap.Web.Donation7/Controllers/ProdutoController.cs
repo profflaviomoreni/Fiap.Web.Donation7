0ea32c1bafd38f6e64ece9a9b2a7b10a345e1865
Flavio Moreni <fmoreni@gmail.com>
2026-05-18 21:53:28 -0300

    criando a funcionalidade de produto

---
+++ Fiap.Web.Donation7/Controllers/ProdutoController.cs
@@ -0,0 +1,61 @@
+﻿using Fiap.Web.Donation7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation7.Controllers
+{
+    public class ProdutoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Lógica para obter a lista de produtos do banco de dados
+            // SELECT * FROM Produtos
+
+            // retornar a lista de produtos para a view
+
+            return View();
+        }
+
+
+        private List<ProdutoModel> ListarProdutosMock() {
+            var produtos = new List<ProdutoModel>{
+                new ProdutoModel()
+                {
+                    ProdutoId = 1,
+                    NomeProduto = "Iphone 11",
+                    CategoriaId = 1,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+                new ProdutoModel()
+                {
+                    ProdutoId = 2,
+                    NomeProduto = "Iphone 12",
+                    CategoriaId = 2,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+                new ProdutoModel()
+                {
+                    ProdutoId = 3,
+                    NomeProduto = "Iphone 13",
+                    CategoriaId = 1,
+                    Disponivel = true,
+                    DataExpiracao = DateTime.Now,
+                },
+                new ProdutoModel()
+                {
+                    ProdutoId = 4,
+                    NomeProduto = "Iphone 14",
+                    CategoriaId = 1,
+                    Disponivel = false,
+                    DataExpiracao = DateTime.Now,
+                },
+            };
+
+            return produtos;
+        } 
+
+
+
+    }
+}
---
+++ Fiap.Web.Donation7/Models/ProdutoModel.cs
@@ -0,0 +1,27 @@
+﻿namespace Fiap.Web.Donation7.Models
+{
+    public class ProdutoModel
+    {
+
+        public int ProdutoId { get; set; }
+
+        public string NomeProduto { get; set; }
+
+        public string Descricao { get; set; }
+
+        public string SugestaoTroca { get; set; }
+
+        public bool Disponivel { get; set; }
+
+        public double Valor { get; set; }
+
+        public DateTime DataCadastro { get; set; }
+
+        public DateTime DataExpiracao { get; set; }
+
+        public int CategoriaId { get; set; }
+
+        public int UsuarioId { get; set; }
+
+    }
+}
