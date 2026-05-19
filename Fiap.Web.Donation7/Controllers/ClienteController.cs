4bd58ea0c1cb0e120daaf8e0e2af80b9eed39bb3
Flavio Moreni <fmoreni@gmail.com>
2026-05-18 21:37:10 -0300

    criando o controller e funcionalidade de cliente

---
+++ Fiap.Web.Donation7/Controllers/ClienteController.cs
@@ -0,0 +1,36 @@
+﻿using Fiap.Web.Donation7.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Fiap.Web.Donation7.Controllers
+{
+    public class ClienteController : Controller
+    {
+
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult Cadastrar(ClienteModel cliente)
+        {
+            //var nome = Request.Form["nome"];
+            //var sobrenome = Request.Form["sobrenome"];
+
+            // ... processamento dos dados, como salvar no banco de dados, etc.
+            // INSERT into Cliente (Nome, Sobrenome) VALUES (nome, sobrenome)
+
+            return View("Sucesso");
+        }
+
+
+        [HttpGet]
+        public IActionResult Help()
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Fiap.Web.Donation7/Models/ClienteModel.cs
@@ -0,0 +1,16 @@
+﻿namespace Fiap.Web.Donation7.Models
+{
+    public class ClienteModel
+    {
+
+        public string Nome { get; set; }
+
+        public string Sobrenome { get; set; }
+
+        public string Email { get; set; }
+
+        public string Telefone { get; set; }
+
+        public DateOnly Nascimento { get; set; }
+    }
+}
